53be638888d3c41d813e48c83283edc871ea2263
moeka802 <moeka802.redapple@gmail.com>
2020-06-12 10:58:57 +0900

    react-routerを使用する

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Switch } from "react-router"
+import Home from "./components/Home"
+import About from "./components/About"
+import Dashboard from "./components/Dashboard"
+import { createBrowserHistory } from "history"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={createBrowserHistory()}>
+      <div>{document.title}</div>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/dashboard"><Dashboard /></Route>
+      </Switch>
+      <a href="/">Back To Home</a>
+    </Router> 
+  )
 }
 
 export default App;
